35bd8041557e6f0a4fcffac2b61a00af98788b50
d1av <davi4alves@gmail.com>
2023-05-10 12:37:24 -0300

    feat: :sparkles: add new stucture

--- wp-content/themes/protocols/page-home.php
+++ wp-content/themes/protocols/page-home.php
@@ -1,195 +1,159 @@
-<?php
-get_header();
-?>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-<div class="limiter">
-	<div class="container-table100">
-		<div class="wrap-table100">
-			<div class="table100 ver3 m-b-110">
-				<div class="table100-head">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Table V01</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="<?= get_template_directory_uri() ?>/img/favicon.ico">
+
+	<!-- header scripts start -->
+	<?php wp_head(); ?>
+	<!-- header scripts end -->
+</head>
+
+<body>
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100">
 					<table>
 						<thead>
-							<tr class="row100 head">
-								<th class="cell100 column1">Class name</th>
-								<th class="cell100 column2">Type</th>
-								<th class="cell100 column3">Hours</th>
-								<th class="cell100 column4">Trainer</th>
-								<th class="cell100 column5">Spots</th>
+							<tr class="table100-head">
+								<th class="column1">Date</th>
+								<th class="column2">Order ID</th>
+								<th class="column3">Name</th>
+								<th class="column4">Price</th>
+								<th class="column5">Quantity</th>
+								<th class="column6">Total</th>
 							</tr>
 						</thead>
-					</table>
-				</div>
-				<div class="table100-body js-pscroll ps ps--active-y">
-					<table>
 						<tbody>
-							<tr class="row100 body">
-								<td class="cell100 column1">Like a butterfly</td>
-								<td class="cell100 column2">Boxing</td>
-								<td class="cell100 column3">9:00 AM - 11:00 AM</td>
-								<td class="cell100 column4">Aaron Chapman</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-29 01:22</td>
+								<td class="column2">200398</td>
+								<td class="column3">iPhone X 64Gb Grey</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Mind &amp; Body</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Adam Stewart</td>
-								<td class="cell100 column5">15</td>
+							<tr>
+								<td class="column1">2017-09-28 05:57</td>
+								<td class="column2">200397</td>
+								<td class="column3">Samsung S8 Black</td>
+								<td class="column4">$756.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$756.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Crit Cardio</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">9:00 AM - 10:00 AM</td>
-								<td class="cell100 column4">Aaron Chapman</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-26 05:57</td>
+								<td class="column2">200396</td>
+								<td class="column3">Game Console Controller</td>
+								<td class="column4">$22.00</td>
+								<td class="column5">2</td>
+								<td class="column6">$44.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Wheel Pose Full Posture</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">7:00 AM - 8:30 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">15</td>
+							<tr>
+								<td class="column1">2017-09-25 23:06</td>
+								<td class="column2">200392</td>
+								<td class="column3">USB 3.0 Cable</td>
+								<td class="column4">$10.00</td>
+								<td class="column5">3</td>
+								<td class="column6">$30.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Playful Dancer's Flow</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-24 05:57</td>
+								<td class="column2">200391</td>
+								<td class="column3">Smartwatch 4.0 LTE Wifi</td>
+								<td class="column4">$199.00</td>
+								<td class="column5">6</td>
+								<td class="column6">$1494.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Zumba Dance</td>
-								<td class="cell100 column2">Dance</td>
-								<td class="cell100 column3">5:00 PM - 7:00 PM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">20</td>
+							<tr>
+								<td class="column1">2017-09-23 05:57</td>
+								<td class="column2">200390</td>
+								<td class="column3">Camera C430W 4k</td>
+								<td class="column4">$699.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$699.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Cardio Blast</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">5:00 PM - 7:00 PM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-22 05:57</td>
+								<td class="column2">200389</td>
+								<td class="column3">Macbook Pro Retina 2017</td>
+								<td class="column4">$2199.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$2199.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Pilates Reformer</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-21 05:57</td>
+								<td class="column2">200388</td>
+								<td class="column3">Game Console Controller</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Supple Spine and Shoulders</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">6:30 AM - 8:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">15</td>
+							<tr>
+								<td class="column1">2017-09-19 05:57</td>
+								<td class="column2">200387</td>
+								<td class="column3">iPhone X 64Gb Grey</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Yoga for Divas</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">9:00 AM - 11:00 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">20</td>
+							<tr>
+								<td class="column1">2017-09-18 05:57</td>
+								<td class="column2">200386</td>
+								<td class="column3">iPhone X 64Gb Grey</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Virtual Cycle</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">20</td>
+							<tr>
+								<td class="column1">2017-09-22 05:57</td>
+								<td class="column2">200389</td>
+								<td class="column3">Macbook Pro Retina 2017</td>
+								<td class="column4">$2199.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$2199.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Like a butterfly</td>
-								<td class="cell100 column2">Boxing</td>
-								<td class="cell100 column3">9:00 AM - 11:00 AM</td>
-								<td class="cell100 column4">Aaron Chapman</td>
-								<td class="cell100 column5">10</td>
+							<tr>
+								<td class="column1">2017-09-21 05:57</td>
+								<td class="column2">200388</td>
+								<td class="column3">Game Console Controller</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Mind &amp; Body</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Adam Stewart</td>
-								<td class="cell100 column5">15</td>
+							<tr>
+								<td class="column1">2017-09-19 05:57</td>
+								<td class="column2">200387</td>
+								<td class="column3">iPhone X 64Gb Grey</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Crit Cardio</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">9:00 AM - 10:00 AM</td>
-								<td class="cell100 column4">Aaron Chapman</td>
-								<td class="cell100 column5">10</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Wheel Pose Full Posture</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">7:00 AM - 8:30 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">15</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Playful Dancer's Flow</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">10</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Zumba Dance</td>
-								<td class="cell100 column2">Dance</td>
-								<td class="cell100 column3">5:00 PM - 7:00 PM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">20</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Cardio Blast</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">5:00 PM - 7:00 PM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">10</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Pilates Reformer</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">10</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Supple Spine and Shoulders</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">6:30 AM - 8:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">15</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Yoga for Divas</td>
-								<td class="cell100 column2">Yoga</td>
-								<td class="cell100 column3">9:00 AM - 11:00 AM</td>
-								<td class="cell100 column4">Donna Wilson</td>
-								<td class="cell100 column5">20</td>
-							</tr>
-							<tr class="row100 body">
-								<td class="cell100 column1">Virtual Cycle</td>
-								<td class="cell100 column2">Gym</td>
-								<td class="cell100 column3">8:00 AM - 9:00 AM</td>
-								<td class="cell100 column4">Randy Porter</td>
-								<td class="cell100 column5">20</td>
+							<tr>
+								<td class="column1">2017-09-18 05:57</td>
+								<td class="column2">200386</td>
+								<td class="column3">iPhone X 64Gb Grey</td>
+								<td class="column4">$999.00</td>
+								<td class="column5">1</td>
+								<td class="column6">$999.00</td>
 							</tr>
 						</tbody>
 					</table>
-					<div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-						<div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-					</div>
-					<div class="ps__rail-y" style="top: 0px; height: 585px; right: 5px;">
-						<div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 293px;"></div>
-					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<?php
-get_footer();
-?>
+
+	<!-- footer scripts start -->
+	<?php wp_footer(); ?>
+	<!-- footer scripts end -->
+</body>
+
+</html>
