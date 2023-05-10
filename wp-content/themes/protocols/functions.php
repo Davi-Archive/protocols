3f7c70d634d534904d750b967c55c0eb3893fd12
d1av <davi4alves@gmail.com>
2023-05-10 00:51:54 -0300

    feat: :sparkles: structure builded

---
+++ wp-content/themes/protocols/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function load_scripts_for_the_site()
+{
+    // stylesheets
+    wp_register_style('register-style', get_stylesheet_uri());
+    wp_enqueue_style('register-style');
+
+    // javascript
+
+   // wp_enqueue_script(get_template_directory_uri() . '/js/bootstrap.min.js');
+
+}
+
+add_action('wp_enqueue_scripts', 'load_scripts_for_the_site');
+
+
+
+
+// Funçoes para Limpar o Header
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'food_links_extra', 3);
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'print_emoji_detection_script');
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+
+// Habilitar Menu
+add_theme_support('menus');
+
+// Registrar Menu
+function register_my_menu()
+{
+    register_nav_menus(array(
+        'menu_principal' => __('Menu Principal'),
+    ));
+}
+add_action('init', 'register_my_menu');
---
+++ wp-content/themes/protocols/index.php
@@ -0,0 +1,16 @@
+<?php
+// Template: Home
+get_header();
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <h1><?php the_title() ?></h1>
+        <main><?php the_content() ?></main>
+
+<?php endwhile;
+else : endif; ?>
+
+<?php
+get_footer();
+?>
